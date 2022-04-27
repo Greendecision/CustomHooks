da602abdab3c251ebd93df3f8c8ea1e6cf4019c7
Fabio Rosada <r.fabio95@gmail.com>
2022-04-27 16:36:10 +0200

    Fixed useCookie function

--- useCookie.ts
+++ useCookie.ts
@@ -23,7 +23,8 @@ export const useCookie = (
   const [value, setValue] = useState<string | object | null>(() => {
     const cookie = Cookies.get(key);
     if (!cookie) {
-      Cookies.set(key, defaultValue, { ...defaultOptions, ...options });
+      const dv = typeof defaultValue === "object" ? JSON.stringify(defaultValue) : defaultValue;
+      Cookies.set(key, dv, { ...defaultOptions, ...options });
       return defaultValue;
     }
     try {
@@ -39,7 +40,8 @@ export const useCookie = (
 
   const updateCookie: (newValue: string | object, options?: Cookies.CookieAttributes) => void = useCallback(
     (newValue, options) => {
-      Cookies.set(key, newValue, { ...defaultOptions, ...options });
+      const nv = typeof newValue === "object" ? JSON.stringify(newValue) : newValue;
+      Cookies.set(key, nv, { ...defaultOptions, ...options });
       setValue(newValue);
     },
     [key]
