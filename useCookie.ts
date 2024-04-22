6b5efaf0a7338562944e97febfb965605cd5ba98
Fabio Rosada <r.fabio95@gmail.com>
2024-04-22 15:52:13 +0200

    BREAKING CHANGES: updated useCookie, added interval to keep the variable udpated
    Before there was the optional param options, that took
    Cookies.Attributes, now it's inside options: { interval: number,
    cookiesOptions: Cookies.Attributes }
    
    This was done to make it easy to pass only the necessary options, for
    example to set the interval without setting the cookiesOptions

--- useCookie.ts
+++ useCookie.ts
@@ -1,5 +1,5 @@
-import { useState, useCallback } from "react";
 import Cookies from "js-cookie";
+import { useCallback, useEffect, useState } from "react";
 
 const defaultOptions: Cookies.CookieAttributes = {
   sameSite: "Strict",
@@ -7,29 +7,55 @@ const defaultOptions: Cookies.CookieAttributes = {
   expires: new Date("9999-12-31"),
 };
 
+interface optionsType {
+  interval?: number;
+  cookieOptions?: Cookies.CookieAttributes;
+}
+
+type updateCookieFunctionType = (
+  newValue: string | object,
+  options?: optionsType,
+) => void;
+type deleteCookieFunctionType = () => void;
+
+type useCookieType = (
+  key: string,
+  defaultValue: string | object,
+  options?: optionsType,
+) => [
+  string | object | null,
+  updateCookieFunctionType,
+  deleteCookieFunctionType,
+];
+
 /**
  * useState like hook
  * that under the hood uses the browser cookies API to store the value across multiple sessions
  *
  * @param key key used to store and retrieve the cookie
  * @param defaultValue default value used in case the cookie doesn't exist yet
- * @param options Cookies.CookieAttribute object passed to the Cookies API
+ * @param options object containing interval and cookieOptions, if interval is a positive number, it will check for changes every n milliseconds
  * @returns array of [value, setValue, deleteValue]
  */
-export const useCookie = (
+export const useCookie: useCookieType = (
   key: string,
   defaultValue: string | object,
-  options: Cookies.CookieAttributes = defaultOptions
-): [
-  string | object | null,
-  (newValue: string | object, options?: Cookies.CookieAttributes) => void,
-  () => void
-] => {
-  const [value, setValue] = useState<string | object | null>(() => {
+  options: optionsType = {
+    interval: -1,
+    cookieOptions: defaultOptions,
+  },
+) => {
+  const { interval, cookieOptions } = options;
+
+  /** Actual function that gets the cookie value, with specified options if present. */
+  const getCookie = useCallback(() => {
     const cookie = Cookies.get(key);
     if (!cookie) {
-      const dv = typeof defaultValue === "object" ? JSON.stringify(defaultValue) : defaultValue;
-      Cookies.set(key, dv, { ...defaultOptions, ...options });
+      const dv =
+        typeof defaultValue === "object"
+          ? JSON.stringify(defaultValue)
+          : defaultValue;
+      Cookies.set(key, dv, { ...defaultOptions, ...cookieOptions });
       return defaultValue;
     }
     try {
@@ -41,18 +67,35 @@ export const useCookie = (
       return cookie;
     }
     return cookie;
-  });
+  }, [key, defaultValue, cookieOptions]);
 
-  const updateCookie: (newValue: string | object, options?: Cookies.CookieAttributes) => void = useCallback(
-    (newValue, options) => {
-      const nv = typeof newValue === "object" ? JSON.stringify(newValue) : newValue;
-      Cookies.set(key, nv, { ...defaultOptions, ...options });
+  /** Set the initial state */
+  const [value, setValue] = useState<string | object | null>(() => getCookie());
+
+  /** if an interval value is specified, every n-milliseconds, it checks if the value changed */
+  useEffect(() => {
+    if (!interval || interval < 0) return;
+    const cookieInterval = setInterval(() => {
+      const newValue = getCookie();
+      setValue(newValue);
+    }, interval);
+
+    return () => clearInterval(cookieInterval);
+  }, [interval, getCookie]);
+
+  /** function to update the cookie value */
+  const updateCookie: updateCookieFunctionType = useCallback(
+    (newValue, newOptions) => {
+      const nv =
+        typeof newValue === "object" ? JSON.stringify(newValue) : newValue;
+      Cookies.set(key, nv, { ...defaultOptions, ...newOptions?.cookieOptions });
       setValue(newValue);
     },
-    [key]
+    [key],
   );
 
-  const deleteCookie: () => void = useCallback(() => {
+  /** function to delete the cookie value */
+  const deleteCookie: deleteCookieFunctionType = useCallback(() => {
     Cookies.remove(key);
     setValue(null);
   }, [key]);
